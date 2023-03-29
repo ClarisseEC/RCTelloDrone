3d043cb742a26e8e28de8a5ec88ca56c0b6e32e7
ClarisseEC <100243132+ClarisseEC@users.noreply.github.com>
2023-03-29 17:27:11 -0500

    Update TelloPicture.py

--- TelloPicture.py
+++ TelloPicture.py
@@ -1,39 +1,49 @@
-#
 # --- (c) 02/2020 f41ardu
 #
 # Tello cam using opencv proof of concept
-#
 #
 
 # import opencv 4.2.0
 import cv2
 
-telloVideo = cv2.VideoCapture("udp://@0.0.0.0:11111")
+# Open the video capture using the 'with' statement
+with cv2.VideoCapture("udp://@0.0.0.0:11111") as telloVideo:
+    # Check if the video capture is open
+    if not telloVideo.isOpened():
+        print("Could not open video capture")
+        exit()
 
-# wait for frame
-ret = False
-# scale down
-scale = 3
+    # Scale down
+    scale = 3
 
-while (True):
-    # Capture frame-by-framestreamon
+    # Read the first frame to get its size
     ret, frame = telloVideo.read()
-    if (ret):
-        # Our operations on the frame come here
-        height, width, layers = frame.shape
-        new_h = int(height / scale)
-        new_w = int(width / scale)
-        resize = cv2.resize(frame, (new_w, new_h))  # <- resize for improved performance
-        # Display the resulting frame
-        cv2.imshow('Tello', resize)
+    if not ret:
+        print("Could not read first frame")
+        exit()
 
-    if cv2.waitKey(1) & 0xFF == ord('s'):
-        cv2.imwrite("test.jpg", resize)  # writes image test.bmp to disk
-        print("Take Picture")
+    # Resize the image for improved performance
+    height, width, layers = frame.shape
+    new_h = int(height / scale)
+    new_w = int(width / scale)
+    resize = cv2.resize(frame, (new_w, new_h))
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # Loop until 'q' is pressed
+    while True:
+        # Capture frame-by-frame
+        ret, frame = telloVideo.read()
+        if frame is not None:
+            # Display the resulting frame
+            cv2.imshow('Tello', resize)
 
-# When everything done, release the capture
-telloVideo.release()
+            # Write the image to disk if 's' is pressed
+            if cv2.waitKey(1) & 0xFF == ord('s'):
+                cv2.imwrite("test.jpg", resize)
+                print("Take Picture")
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+# Release the capture and close the window
 cv2.destroyAllWindows()
